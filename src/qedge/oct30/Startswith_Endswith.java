9a81452fcfb5a85e3f2893d3931b2096bc76341d
pavani <kalthireddypavani@gmail.com>
2019-11-14 07:45:04 +0530

    upto november14 scripts

---
+++ src/qedge/oct30/Startswith_Endswith.java
@@ -0,0 +1,13 @@
+package qedge.oct30;
+
+public class Startswith_Endswith {
+
+	public static void main(String[] args) {
+		String str1="where are you?";
+	    System.out.println(str1.startsWith("where"));
+		System.out.println(str1.startsWith("Are"));
+		System.out.println(str1.endsWith("you"));
+		System.out.println(str1.endsWith("you?"));
+	}
+
+}
